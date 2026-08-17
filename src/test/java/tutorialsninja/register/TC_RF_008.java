1cb6684e7a560ea035220b97e9bf6466bbea5218
Dipali <dipalipatil8390@gmail.com>
2026-08-17 19:58:48 +0530

    TC_RF_008 Test case automated

---
+++ src/test/java/tutorialsninja/register/TC_RF_008.java
@@ -0,0 +1,48 @@
+package tutorialsninja.register;
+
+import java.time.Duration;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_RF_008 {
+
+	@Test
+	public void verifyRegistringAccountBySayingYesToNewsletter() {
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo");
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+
+		driver.findElement(By.id("input-firstname")).sendKeys("dipali");
+		driver.findElement(By.id("input-lastname")).sendKeys("patil");
+		driver.findElement(By.id("input-email")).sendKeys(generateNewEmail());
+		driver.findElement(By.id("input-telephone")).sendKeys("4456567654");
+		driver.findElement(By.id("input-password")).sendKeys("12345");
+		driver.findElement(By.id("input-confirm")).sendKeys("1234567");
+		driver.findElement(By.xpath("//input[@name='newsletter'][@value='1']")).click();
+		driver.findElement(By.name("agree")).click();
+		driver.findElement(By.xpath("//input[@value=\"Continue\"]")).click();
+		
+		String expectedWarningMsg="Password confirmation does not match password!";
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-confirm']/following-sibling::div")).getText(), expectedWarningMsg);
+		
+		
+
+	
+	
+	}
+
+	public String generateNewEmail() {
+		return new Date().toString().replaceAll(" ", "").replaceAll("\\:", "") + "@gmail.com";
+
+	}
+}
